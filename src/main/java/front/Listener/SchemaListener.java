79a5a8898bb2e781c16cbe123d4bd67629b6b5aa
sgil0 <samuel.gillon@protonmail.com>
2026-01-16 09:42:49 +0100

    Ajout Listener

---
+++ src/main/java/front/Listener/SchemaListener.java
@@ -0,0 +1,9 @@
+package front.Listener;
+
+import back.TypeIntervention;
+import java.util.List;
+
+public interface SchemaListener {
+    // Appelé chaque fois qu'on coche/décoche un truc sur le dessin
+    void onSelectionChanged(List<TypeIntervention> selection);
+}
---
+++ src/main/java/front/Listener/VehiculeSelectionListener.java
@@ -0,0 +1,5 @@
+package front.Listener;
+
+public interface VehiculeSelectionListener {
+    void onVehiculeSelected(String immatriculation);
+}
